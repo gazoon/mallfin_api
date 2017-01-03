38116c612bd73e67f7ffda62613f2d85a3d440e0
gazoon <g89250741413@gmail.com>
2017-01-03 17:40:14 +0300

    add mall creation

--- db/models/models.go
+++ db/models/models.go
@@ -2,21 +2,41 @@ package models
 
 import (
 	"database/sql"
-	log "github.com/Sirupsen/logrus"
 	"mallfin_api/db"
+
+	log "github.com/Sirupsen/logrus"
 )
 
 var (
 	moduleLog = log.WithField("location", "models")
 )
 
+type Location struct {
+	Lat float64 `json:"lat"`
+	Lon float64 `json:"lon"`
+}
 type Mall struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Address     string `json:"string"`
-	DayAndNight bool   `json:"day_and_night"`
+	ID          int      `json:"id"`
+	Name        string   `json:"name"`
+	Address     string   `json:"address"`
+	Site        *string  `json:"site"`
+	DayAndNight bool     `json:"day_and_night"`
+	Location    Location `json:"location"`
+	CityID      int      `json:"-"`
 }
 
+func CreateMall(mall *Mall) *Mall {
+	conn := db.GetConnection()
+	err := conn.QueryRow(`
+	INSERT INTO mall (name, site, address, day_and_night, city_id, location)
+	VALUES ($1, $2, $3, $4, $5, ST_SETSRID(ST_POINT($6, $7), 4326))
+	RETURNING id`, mall.Name, mall.Site, mall.Address, mall.DayAndNight, mall.CityID, mall.Location.Lat, mall.Location.Lon).Scan(&mall.ID)
+	if err != nil {
+		moduleLog.WithField("mall", mall).Panicf("Cannot craete mall: %s", err)
+	}
+	return mall
+
+}
 func GetMall(mallID int) *Mall {
 	conn := db.GetConnection()
 	mall := Mall{}
@@ -25,9 +45,12 @@ func GetMall(mallID int) *Mall {
 	  id,
 	  name,
 	  address,
-	  day_and_night
+	  site,
+	  day_and_night,
+	  ST_X(location),
+	  ST_Y(location)
 	FROM mall
-	WHERE id = $1`, mallID).Scan(&mall.ID, &mall.Name, &mall.Address, &mall.DayAndNight)
+	WHERE id = $1`, mallID).Scan(&mall.ID, &mall.Name, &mall.Address, &mall.Site, &mall.DayAndNight, &mall.Location.Lat, &mall.Location.Lon)
 	if err == sql.ErrNoRows {
 		return nil
 	} else if err != nil {
